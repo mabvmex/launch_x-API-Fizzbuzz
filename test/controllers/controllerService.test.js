caa419319f7b832a19a9d178533494815de43e0b
mabvmex <mabvmex@gmail.com>
2022-05-08 23:38:28 -0500

    Pruebas para explorerController en ExplorerController.test completas y funcionando

--- test/controllers/controllerService.test.js
+++ test/controllers/controllerService.test.js
@@ -12,4 +12,28 @@ describe("Unit Test for controllerService", () => {
         
         expect(explorerServiceFiltrado[0]).toBe("Woopa1");
     });
+
+    test("2.- getExplorersUsernamesByMission", () => {
+        const explorer = Reader.readJsonFile("explorers.json");
+        const mission = "node";
+        const usernamesFiltrado = ExplorerService.getExplorersUsernamesByMission(
+            explorer,
+            mission
+        );
+
+        expect(usernamesFiltrado[usernamesFiltrado.length-1]).toBe("ajolonauta15");
+    });
+
+    test("3.- getAmountOfExplorersByMission", () => {
+        const explorer = Reader.readJsonFile("explorers.json");
+        const mission = "node";
+        const amountofExplorers = ExplorerService.getAmountOfExplorersByMission(
+            explorer,
+            mission
+        );
+
+        expect(amountofExplorers).toBe(10);
+    });
+
+    
 });
