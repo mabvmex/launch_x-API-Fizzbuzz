aaae16a370de9d13e342f21b75c917798b57ed55
mabvmex <mabvmex@gmail.com>
2022-05-09 16:06:13 -0500

    update server.js + test/server

--- app/lib/server.js
+++ app/lib/server.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 app.get("/", (req, res) => {
     res.json({
-        message: "=== FizzBuzz API welcome! ==="
+        message: "=== FizzBuzz API welcome! ===",
     });
 });
 
@@ -23,14 +23,20 @@ app.get("/v1/explorer/:mission", (req, res) => {
 
 app.get("/v1/explorer/usernames/:mission", (req, res) => {
     const mission = req.params.mission;
-    const explorersUsernames = ExplorerController.getExplorersUsernamesByMission(mission);
-    res.json({mission: req.params.mission, usernames:explorersUsernames});
+    const explorersUsernames =
+        ExplorerController.getExplorersUsernamesByMission(mission);
+    res.json({ mission: req.params.mission, usernames: explorersUsernames });
 });
 
 app.get("/v1/explorer/amount/:mission", (req, res) => {
     const mission = req.params.mission;
-    const amountOfExplorers = ExplorerController.getExplorersAmonutByMission(mission);
-    res.json({mission: req.params.mission, amountOfExplorers: amountOfExplorers});
+    const amountOfExplorers =
+        ExplorerController.getExplorersAmonutByMission(mission);
+    res.json({
+        mission: req.params.mission,
+        amountOfExplorers: amountOfExplorers,
+    });
 });
+
 
 module.exports = app;
--- test/utils/server.test.js
+++ test/utils/server.test.js
@@ -1,31 +1,44 @@
-const app = "./../../app/lib/server.js";
-const request = require("supertest");
+// describe("Unit test for Server", () => {
+//     test("1.- Testing to see if Jest works", (done) => {
+        
+//         const app = "./../../app/lib/server.js";
+//         const supertest = require("supertest");
+//         const req = supertest(app);
+        
+        
+//         const res = req.get("/test");
 
-describe("Unit test for Server", () => {
-    test("Get '/v1/explorer", (done) => {
-        request(app)
-            .get("localhost:3000/v1/explorer/")
-            .expect(200)
-            .expect((res) => {
-                expect(res.body).toBe("hola");
-            })
-            .end((err, res) => {
-                if (err) return done(err);
-                return done();
-            });
-    });
+//         expect(res.status).toBe(200);
+//         // expect(res.body.message).toBe("PASS!!!");
+//         done();
+//     });
+// });
 
-    // test("2.- GET /v1/explorer/mission", (done) => {
-    //     request(app)
-    //         .get("/launchX")
-    //         .expect(200)
-    //         .expect((res) => {
-    //             console.log(res.text);
-    //             expect(res.text).toBe("launchX");
-    //         })
-    //         .end((err, res) => {
-    //             if (err) return done(err);
-    //             return done;
-    //         });
-    // });
-});
+// describe("Unit test for Server", () => {
+//     it("2.- Get '/v1/explorer", (done) => {
+//         request(app)
+//             .get("/v1/explorer/node")
+//             .expect(200)
+//             .expect((res) => {
+//                 expect(res.json).toBe("hola");
+//             })
+//             .end((err, res) => {
+//                 if (err) return done(err);
+//                 return done();
+//             });
+//     });
+
+// test("2.- GET /v1/explorer/mission", (done) => {
+//     request(app)
+//         .get("/launchX")
+//         .expect(200)
+//         .expect((res) => {
+//             console.log(res.text);
+//             expect(res.text).toBe("launchX");
+//         })
+//         .end((err, res) => {
+//             if (err) return done(err);
+//             return done;
+//         });
+// });
+// });
